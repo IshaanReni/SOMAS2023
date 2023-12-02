36779c1cbf8621d3216633f87c78e63a5dc4da02
Alexandra Neagu <33195033+neagualexa@users.noreply.github.com>
2023-12-02 12:22:13 +0000

    feat: Messaging and forces messages (#14)
    * introducing messaging from the PR on 1dec
    
    * forces messages introduced and tested in the messaging_test

--- internal/common/objects/tests/messaging_test.go
+++ internal/common/objects/tests/messaging_test.go
@@ -15,6 +15,7 @@ type ExtendedBaseBiker struct {
 	*obj.BaseBiker       // BaseBiker inherits functions from BaseAgent such as GetID(), GetAllMessages() and UpdateAgentInternalState()
 	OtherBiker           obj.IBaseBiker
 	OtherBikerReputation float64
+	OtherBikerForces     []utils.Forces
 }
 
 // Produce new IExtendedBaseBiker
@@ -33,10 +34,25 @@ func (ebb *ExtendedBaseBiker) HandleReputationMessage(msg obj.ReputationOfAgentM
 	}
 }
 
+func (ebb *ExtendedBaseBiker) HandleForcesMessage(msg obj.ForcesMessage) {
+	// Team's agent should implement logic for handling other biker messages that were sent to them.
+
+	// sender := msg.BaseMessage.GetSender()
+	agentId := msg.AgentId
+	agentForces := msg.AgentForces
+
+	if agentId == ebb.OtherBiker.GetID() {
+		ebb.OtherBikerForces = append(ebb.OtherBikerForces, agentForces)
+		fmt.Println(len(ebb.OtherBikerForces), ebb.OtherBikerForces)
+	}
+
+}
+
 // Send messages to server which will be sent to the reciepients of the messages
 func (ebb *ExtendedBaseBiker) GetAllMessages([]obj.IBaseBiker) []messaging.IMessage[obj.IBaseBiker] {
 	reputationMsg := ebb.CreateReputationMessage()
-	return []messaging.IMessage[obj.IBaseBiker]{reputationMsg}
+	forcesMsg := ebb.CreateForcesMessage()
+	return []messaging.IMessage[obj.IBaseBiker]{reputationMsg, forcesMsg}
 }
 
 func (ebb *ExtendedBaseBiker) CreateReputationMessage() obj.ReputationOfAgentMessage {
@@ -48,6 +64,18 @@ func (ebb *ExtendedBaseBiker) CreateReputationMessage() obj.ReputationOfAgentMes
 		BaseMessage: messaging.CreateMessage[obj.IBaseBiker](ebb, updatedAgents),
 		AgentId:     ebb.OtherBiker.GetID(),
 		Reputation:  0.1,
+	}
+}
+
+func (ebb *ExtendedBaseBiker) CreateForcesMessage() obj.ForcesMessage {
+	// Currently this returns a default message which sends to all bikers on the biker agent's bike
+	// For team's agent, add your own logic to communicate with other agents
+	var updatedAgents []obj.IBaseBiker
+	updatedAgents = append(updatedAgents, ebb.OtherBiker)
+	return obj.ForcesMessage{
+		BaseMessage: messaging.CreateMessage[obj.IBaseBiker](ebb, updatedAgents),
+		AgentId:     ebb.OtherBiker.GetID(),
+		AgentForces: ebb.GetForces(),
 	}
 }
 
@@ -63,8 +91,15 @@ func TestBaseBikerMessaging(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		msgs1 := biker1.GetAllMessages(bikers)
 		msgs2 := biker2.GetAllMessages(bikers)
+		fmt.Println("iteration ", i)
+		fmt.Println("msg biker 1: ", msgs1)
+		fmt.Println("msg biker 2: ", msgs2)
 		msgs1[0].InvokeMessageHandler(biker1)
 		msgs2[0].InvokeMessageHandler(biker2)
+
+		msgs1[1].InvokeMessageHandler(biker1)
+		msgs2[1].InvokeMessageHandler(biker2)
+
 	}
 
 	// Assert that biker1.OtherBikerReputation and biker2.OtherBikerReputation are both equal to 5
